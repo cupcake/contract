2d92651ddfc2d87f81f55f62ec30dee8f8fbb477
Cole Noreika <64798128+0xCold@users.noreply.github.com>
2023-04-05 17:34:26 -0400

    add tests for new refillable totalSupply logic

--- solana/tests/refillable1Of1.ts
+++ solana/tests/refillable1Of1.ts
@@ -1,23 +1,29 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
-import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 import { Cupcake } from '../target/types/cupcake';
 import { CupcakeProgram } from "../wip_sdk/cucpakeProgram";
 import { createProgrammableNFT, createRuleSetAccount, mintNFT } from "../wip_sdk/programmableAssets";
 import { Bakery } from '../wip_sdk/state/bakery';
 
-describe('Refillable Sprinkle', () => {
+describe('`Refillable1Of1` Sprinkle', () => {
   anchor.setProvider(anchor.Provider.env());
 
   const admin = anchor.web3.Keypair.generate();
   const user = anchor.web3.Keypair.generate();
+
+  let nftMint: PublicKey | undefined = undefined;
+  let nftMint2: PublicKey | undefined = undefined;
 
   const cupcakeProgram = anchor.workspace.Cupcake as Program<Cupcake>;
   const cupcakeProgramClient = new CupcakeProgram(cupcakeProgram, admin)
 
   const bakeryPDA = Bakery.PDA(admin.publicKey, cupcakeProgram.programId);
 
-  it('Should fund test wallets', async () => {
+  const sprinkleUID = "66554433221155"
+  const sprinkleAuthority = anchor.web3.Keypair.generate();
+
+  it('Should create a bakery', async () => {
     let sig = await cupcakeProgram.provider.connection.requestAirdrop(
       admin.publicKey, 
       LAMPORTS_PER_SOL * 10
@@ -29,14 +35,13 @@ describe('Refillable Sprinkle', () => {
       LAMPORTS_PER_SOL * 10
     );
     await cupcakeProgram.provider.connection.confirmTransaction(sig2, 'singleGossip');
-  })
 
-  it('Tests for a normal NFT', async () => {
-    try {
-    const sprinkleUID = "66554433221155"
-    const sprinkleAuthority = anchor.web3.Keypair.generate();
+    const createBakeryTxHash = await cupcakeProgramClient.createBakery()
+    console.log('createBakeryTxHash', createBakeryTxHash);
+  });
 
-    const nftMint = await mintNFT(
+  it('Should mint 2 non-programmable NFTs', async () => {
+    nftMint = await mintNFT(
       cupcakeProgramClient.program.provider,
       admin,
       admin.publicKey,
@@ -44,9 +49,17 @@ describe('Refillable Sprinkle', () => {
     );
     console.log("nftMint", nftMint.toString());
 
-    const createBakeryTxHash = await cupcakeProgramClient.createBakery()
-    console.log('createBakeryTxHash', createBakeryTxHash);
+    nftMint2 = await mintNFT(
+      cupcakeProgramClient.program.provider,
+      admin,
+      admin.publicKey,
+      0
+    );
+    console.log("nftMint2", nftMint2.toString());
+  });
 
+  it('Should bake a `Refillable1Of1` Sprinkle with the first mint', async () => {
+    try {
     const bakeSprinkleTxHash = await cupcakeProgramClient.bakeSprinkle(
       "refillable1Of1",
       sprinkleUID, 
@@ -56,7 +69,11 @@ describe('Refillable Sprinkle', () => {
       sprinkleAuthority
     );
     console.log('bakeSprinkleTxHash', bakeSprinkleTxHash);
+    }catch(e){console.warn(e)}
+  });
 
+  it('Should claim the `Refillable1Of1` Sprinkle', async () => {
+    try {
     const claimSprinkleTxHash = await cupcakeProgramClient.claimSprinkle(
       sprinkleUID, 
       user.publicKey,
@@ -64,5 +81,30 @@ describe('Refillable Sprinkle', () => {
     );
     console.log('claimSprinkleTxHash', claimSprinkleTxHash);
     }catch(e){console.warn(e)}
-});
+  });
+
+  it('Should rebake the `Refillable1Of1` Sprinkle with the 2nd mint', async () => {
+    try {
+    const bakeSprinkleTxHash = await cupcakeProgramClient.bakeSprinkle(
+      "refillable1Of1",
+      sprinkleUID, 
+      nftMint2, 
+      1, 
+      2, 
+      sprinkleAuthority
+    );
+    console.log('bakeSprinkleTxHash', bakeSprinkleTxHash);
+    }catch(e){console.warn(e)}
+  });
+
+  it('Should claim the rebaked `Refillable1Of1` Sprinkle', async () => {
+    try {
+    const claimSprinkleTxHash = await cupcakeProgramClient.claimSprinkle(
+      sprinkleUID, 
+      user.publicKey,
+      sprinkleAuthority
+    );
+    console.log('claimSprinkleTxHash', claimSprinkleTxHash);
+    }catch(e){console.warn(e)}
+  });
 });
